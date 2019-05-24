a8f0e1f0a2cc27d056e4c4a16edb9de53a5f8533
hantswilliams <hantsawilliams@gmail.com>
2019-05-23 22:02:24 -0400

    fixed producer code

--- kafka_temphumid/producer.py
+++ kafka_temphumid/producer.py
@@ -21,8 +21,8 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=la
 
 while True:
 	#Step 1: Get the data from the sensor 
-    humidity, temperature = Adafruit_DHT.read_retry(22, 22)
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     #Step 2: Send the data to a kafka stream called 'sensordata1'
     producer.send('sensordata1', {'temp': temperature, 'hum': humidity, 'wkt': str(datetime.datetime.now())})
     #Step 3: Put the sensor to sleep for 60 seconds 
-    time.sleep(60)
+    time.sleep(10)
